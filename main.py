54118a9d31a23f432faa7b818658d3facb0db4ed
k21091 <k21091kk@aitech.ac.jp>
2022-12-23 17:01:07 +0900

    htmlの追加

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ def select():
     return render_template('select-egg.html')
 
 #たまごをあたためる画面
-#@app.route('/warm', methods=["GET"])
-#def home_get():
-#    return render_template('warm-egg.html')
+@app.route('/warm', methods=["GET"])
+def home_get():
+    return render_template('warm-egg.html')
 
 #メインのゲーム画面(タスクの選択)
-#@app.route('/game', methods=["GET"])
-#def home_get():
-#    return render_template('game.html')
+@app.route('/game', methods=["GET"])
+def home_get2():
+    return render_template('game.html')
 
 #たまごをあたためる画面
-#@app.route('/finish', methods=["GET"])
-#def home_get():
-#    return render_template('finish.html')
+@app.route('/finish', methods=["GET"])
+def home_get3():
+    return render_template('finish.html')
 if __name__ == '__main__':
     app.run(debug=True)
