1651a6afa106b4a309970414d8760a8c271d9f6a
k21091 <k21091kk@aitech.ac.jp>
2022-12-23 12:10:32 +0900

    最初のコミット

--- main.py
+++ main.py
@@ -5,26 +5,28 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 #ホーム画面
-@app.route('/home', methods=["GET"])
-def home_get():
-    return render_template('home.html')
+@app.route('/', methods=["GET"])
+def title():
+    return render_template('title.html')
 
 #たまごの選択画面
 @app.route('/select', methods=["GET"])
-def home_get():
+def select():
     return render_template('select-egg.html')
 
 #たまごをあたためる画面
-@app.route('/warm', methods=["GET"])
-def home_get():
-    return render_template('warm-egg.html')
+#@app.route('/warm', methods=["GET"])
+#def home_get():
+#    return render_template('warm-egg.html')
 
 #メインのゲーム画面(タスクの選択)
-@app.route('/game', methods=["GET"])
-def home_get():
-    return render_template('game.html')
+#@app.route('/game', methods=["GET"])
+#def home_get():
+#    return render_template('game.html')
 
 #たまごをあたためる画面
-@app.route('/finish', methods=["GET"])
-def home_get():
-    return render_template('finish.html')
+#@app.route('/finish', methods=["GET"])
+#def home_get():
+#    return render_template('finish.html')
+if __name__ == '__main__':
+    app.run(debug=True)
