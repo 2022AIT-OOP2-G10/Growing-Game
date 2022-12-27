18b8b4924722ab5e01a8a15a741d367fd54d13e6
OkamotoAi <okamotoai0217@icloud.com>
2022-12-27 18:46:35 +0900

    [add] たまごから成長した後のゲーム画面を追加

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ hungry=0
 love=0
 #ほこり
 dust=0
+#成長度
+grow=0
 
+#何日目か
+day=1
 
 def food_child():
     hungry = 100
@@ -63,7 +67,18 @@ def home_get():
 #メインのゲーム画面(タスクの選択)
 @app.route('/game', methods=["GET"])
 def home_get2():
-    return render_template('game.html')
+    # クエリを使ってGETメソッドで処理できる(都合悪そうならPOSTにも変更可)
+    task=request.args.get('task')
+    if task == 'eat':
+        # ご飯を食べる関数　とかをここに書く
+        print("ご飯を食べました")
+    elif task == 'play':
+        # 遊ぶ
+        print("遊びました")
+    elif task == 'sleep':
+        # 寝る
+        print("寝ました")
+    return render_template('game.html',day=day,hungry=hungry,dust=dust,grow=grow)
 
 #たまごをあたためる画面
 @app.route('/finish', methods=["GET"])
