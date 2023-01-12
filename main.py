554ea238b328325766a31054fed622f14caa7dee
TowelTowel <x18091xx@aitech.ac.jp>
2023-01-12 15:12:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ grow=0
 #何日目か
 day=1
 
+#各種フラグ
+grow_child = False #卵から子
+grow_adult = False #子から大人
+grow_end = False #育成終了
+death = False #餓死
+runaway = False #逃走
+runaway_count = 0 #逃走までの日数カウント
 class child :
     hungry = 0
     love = 0
@@ -61,10 +68,10 @@ def sleep_child(chi):
     elif chi.hungry > 30:
         chi.hungry = chi.hungry - 30
 
-    if chi.love >= 80:
-        chi.love = 100
-    elif chi.love < 80:
-        chi.love = chi.love + 20
+    if chi.love >= 20:
+        chi.love = chi.love - 20
+    elif chi.love < 20:
+        chi.love = 0
 
 c = child()
 
@@ -119,12 +126,57 @@ def sleep_adult(adu):
     elif adu.hungry > 30:
         adu.hungry = adu.hungry - 30
 
-    if adu.love >= 80:
-        adu.love = 100
-    elif adu.ove < 80:
-        adu.love = adu.love + 20
+    if adu.love >= 20:
+        adu.love = adu.love - 20
+    elif adu.love < 20:
+        adu.love = 0
 
 a = adult()
+
+def day_start(chi, adu):
+    day = day + 1 #日数経過
+    
+    #1日の開始時のパラメータ変動
+    if grow_child == True :
+        chi.dust = chi.dust + 10
+        chi.love = chi.love - 10
+    elif grow_adult == True :
+        adu.dust = adu.dust + 10
+        adu.love = adu.love - 10
+
+def day_end(chi, adu):
+    if day == 1 : #2子に成長フラグを立てる
+        grow_child = True
+    if day == 9 : #大人に成長フラグを立てる
+        grow_adult = True
+        grow_child = False
+    if day == 25 : #終了フラグを立てる
+        grow_end = True
+
+    #死亡フラグ
+    if grow_child == True :
+        if chi.hungry == 0 :
+            death = True
+    elif grow_adult == True :
+        if adu.hungry == 0 :
+            death = True
+
+    #逃走フラグ
+    if grow_child == True :
+        if chi.love < 30 : #好感度30未満でカウント増加
+            runaway_count = runaway_count + 1
+        elif chi.love >= 30 : #好感度30以上でカウントリセット
+            runaway_count = 0
+        if runaway_count == 3 : #カウント3で逃走フラグがたつ
+            runaway = True
+    elif grow_adult == True :
+        if adu.love < 30 :
+            runaway_count = runaway_count + 1
+        elif adu.love >= 30 :
+            runaway_count = 0
+        if runaway_count == 3 :
+            runaway = True
+
 
 
 app = Flask(__name__)
